06c9b1219eb195fd36aab878ba9d10fbd33f958b
rozytwan <rozy.twanju@krennova.com.np>
2017-03-16 09:24:11 +0545

    Upload And Download the same file

---
+++ Upload/Controllers/UploadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Upload.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //for Upload of Files
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            string path = Server.MapPath("~/Files/" + file.FileName);//file path
+            file.SaveAs(path);//save file
+            ViewBag.path = path;
+            return View();
+        }
+
+
+        //for Download of the files which is Uploaded above
+        public ActionResult Downloads()
+        {
+            var dir = new System.IO.DirectoryInfo(Server.MapPath("~/Files/"));
+            System.IO.FileInfo[] fileNames = dir.GetFiles("*.*"); List<string> items = new List<string>();
+            foreach (var file in fileNames)
+            {
+                items.Add(file.Name);
+            }
+            return View(items);
+        }
+
+        public FileResult Download(string ImageName)
+        {
+            var FileVirtualPath = "~/Files/" + ImageName;
+            return File(FileVirtualPath, "application/force-download", Path.GetFileName(FileVirtualPath));
+        }
+    }
+}
---
+++ Upload/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Upload.MvcApplication" Language="C#" %>
